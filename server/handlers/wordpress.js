2e27c96d6939268cf1b3098ca283b0bb275ebe26
Andrei Maxim <andreey_047@yahoo.com>
2018-06-15 10:50:00 +0300

    finalizing

--- server/handlers/wordpress.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (req, res) {
-    let resp = {
-        "wordpress": 1
-    };
-    let data = JSON.stringify(resp);
-    res.writeHead(200, {"content-type": "application/json"});
-    res.write(data);
-    res.end();
-};
